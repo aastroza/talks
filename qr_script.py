d83d498d07aa4d22a3696bcd7d9f53b98c563246
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2025-06-18 01:09:26 -0400

    Update QR code link and save as new image file

--- qr_script.py
+++ qr_script.py
@@ -1,4 +1,4 @@
 import qrcode
-img = qrcode.make('https://github.com/aastroza/talks/tree/main/duoc-summitia-scl')
+img = qrcode.make('https://github.com/aastroza/talks/tree/main/duoc-summitia-valpo')
 type(img)  # qrcode.image.pil.PilImage
-img.save("some_file.png")
+img.save("some_file_2.png")
