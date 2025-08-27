6a9eebcf3999281734f1461d259c5f75fa939bc0
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2025-08-27 16:15:30 -0400

    Update QR code generation to point to the correct conference URL and save as qr_iadevs.png

--- qr_script.py
+++ qr_script.py
@@ -1,4 +1,4 @@
 import qrcode
-img = qrcode.make('https://github.com/aastroza/talks/tree/main/duoc-summitia-valpo')
+img = qrcode.make('https://conferencia.iadevs.cl/')
 type(img)  # qrcode.image.pil.PilImage
-img.save("some_file_2.png")
+img.save("qr_iadevs.png")
