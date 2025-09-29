d3534cf3598a77940a46d73b2cc18b71e774f6f3
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2025-09-29 15:25:10 -0300

    Update QR code generation to point to the correct conference URL and save as qr_iadevs_conce.png

--- qr_script.py
+++ qr_script.py
@@ -1,4 +1,4 @@
 import qrcode
-img = qrcode.make('https://conferencia.iadevs.cl/')
+img = qrcode.make('https://postgrados.udd.cl/programas/conferencia-iadevs-2025-%e2%80%93-concepcion-235269/')
 type(img)  # qrcode.image.pil.PilImage
-img.save("qr_iadevs.png")
+img.save("qr_iadevs_conce.png")
