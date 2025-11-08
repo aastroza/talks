165d35046e188d7c9fd230f83ae673da6318c795
Alonso Astroza Tagle <aastroza@ing.uchile.cl>
2025-11-07 23:06:21 -0300

    Update QR code generation script for 9punto5dev and add corresponding image

--- qr_script.py
+++ qr_script.py
@@ -1,4 +1,4 @@
 import qrcode
-img = qrcode.make('https://github.com/aastroza/talks/tree/main/9punto5')
+img = qrcode.make('https://github.com/aastroza/talks/tree/main/9punto5dev')
 type(img)  # qrcode.image.pil.PilImage
-img.save("qr_9punto5.png")
+img.save("qr_9punto5dev.png")
